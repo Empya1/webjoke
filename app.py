8616d94d64b119af40ec34ee12ea3c372676b5c5
Empya1 <161871238+Empya1@users.noreply.github.com>
2024-10-25 00:23:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,14 @@ app.config["SECRET_KEY"] = os.environ["SECRET_KEY"]
 
 @app.route("/")
 def index(): 
-  return "yay"
+  return """
+<html>
+<head>
+<meta name="monetag" content="9c6b92db72d63d64f7810d7cf8264826">
+</head>
+</html>
+<body>
+<div>Yay</div>
+</body>
+""""
 
